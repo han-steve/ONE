f054dc914f0c6a73d27ad7b41047fa1605abc3be
Steve Han <stevehan2001@gmail.com>
2019-02-17 00:44:04 -0600

    Formatting

--- hackathon/src/components/Dashboard.vue
+++ hackathon/src/components/Dashboard.vue
@@ -5,10 +5,45 @@
       <p class="subtitle">overview of your financial life</p>
       <label>Search by Category: </label>
       <input type="text" v-model="search">
-      <div>
+      <div id="table">
+        <div class="rows">
+              <div class="table-heading">
+                Date
+              </div>
+              <div class="table-heading">
+              </div>
+              <div class="table-heading">
+              </div>
+              <div class="table-heading">
+              </div>
+              <div class="table-heading">
+              </div>
+              <div class="table-heading">
+              </div>
+            </div>
         <ul>
           <li v-for="transaction of list" v-bind:key="transaction['.key']">
-            <p>{{transaction.amount}}</p>
+            <div class="rows">
+              <div class="date">
+                {{transaction.date}}
+              </div>
+              <div class="amount">
+                {{transaction.amount}}
+              </div>
+              <div class="account">
+                {{transaction.account}}
+              </div>
+              <div class="category">
+                {{transaction.category}}
+              </div>
+              <div class="payee">
+                {{transaction.payee}}
+              </div>
+              <div class="memo">
+                {{transaction.memo}}
+              </div>
+            </div>
+            
           </li>
         </ul>
       </div>
@@ -17,7 +52,7 @@
 </template>
 
 <script>
-  var user = "Timo";
+  var user = "Q";
   import {transactions} from '../firebase';
   import {users} from '../firebase';
   export default {
@@ -47,6 +82,21 @@
 </script>
 
 <style>
+  .table-heading {
+    text-align: center;
+
+  }
+  ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+  }
+  .rows {
+    text-align: center;
+    width: 100%;
+    display: grid; 
+    grid-template-columns: repeat(6, auto);
+  }
   main {
     background-color: #F7F7FC;
     padding: 3em;
